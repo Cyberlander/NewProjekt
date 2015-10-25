f8386a7a55764efe1411370e079039f306593a44
kautschuckflasche <nilsw.agner@web.de>
2015-10-25 10:32:02 +0100

    added simple ki for peasant

---
+++ Assets/scripts/PeasantScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeasantScript : MonoBehaviour 
+{	
+	[SerializeField]
+	private GameObject target;
+	[SerializeField]
+	private float speed;
+	private Rigidbody2D rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () 
+	{
+		Vector3 dir = (target.transform.position - transform.position).normalized;
+		Quaternion rotation = Quaternion.LookRotation
+			(target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+		transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
+		if (dir != transform.right) 
+		{
+			rb.MoveRotation(180);
+		}
+		rb.velocity = transform.right * speed;
+	}
+}
